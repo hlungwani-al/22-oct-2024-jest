787de5b5b051ae94de70af39916f03027c377fec
hlungwani-al <hlungwani.al6@gmail.com>
2024-10-22 14:27:51 +0200

    jest

---
+++ __test__/main.test.js
@@ -0,0 +1,19 @@
+
+import { sum, subtract, divide, multiply } from '../main.js'
+
+test ("adding 2 numbers and returning the sum", ()=> {
+    expect(sum(1,2)).toBe(3);
+})
+
+test ("expect subtraction", ()=> {
+    expect(subtract(4,2)).toBe(2);
+})
+
+test ("expect to get multiplication", ()=>{
+    expect(multiply(3, 4)).toBe(12);
+})
+
+test ("deny division by zero", ()=> {
+    expect(divide(4, 0)).toThrow("Division by zero is not allowed");
+})
+
---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+export function sum (a, b) {
+    return a + b;
+}
+
+export function subtract (c, d) {
+    return c - d;
+}
+
+export function multiply (e, f) {
+    return e * f;
+}
+
+export function divide (g, h){
+    if (h==0)
+        throw new Error ("Division by zero is not allowed");
+    return g / h;
+}
